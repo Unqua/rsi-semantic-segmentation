4160151adf0d463e73a0fa8dac4cb800cc510bba
张天舒 <332755351@qq.com>
2022-01-09 14:12:31 +0800

    hopefully this unet works

--- models/brrnet.py
+++ models/brrnet.py
@@ -14,19 +14,15 @@ class PredictModule(nn.Module):
                  num_classes: int,
                  merge_way: str, with_bn: bool):
         super(PredictModule, self).__init__()
-        self.in_channels = in_channels
-        self.encoder_channels = encoder_channels
-        self.center_channels = center_channels
-        self.decoder_channels = decoder_channels
-        self.num_classes = num_classes
-        self.merge_way = merge_way
-        self.with_bn = with_bn
 
-        self.encoder = UNetEncoder(self.in_channels, self.encoder_channels, with_bn=self.with_bn)
-        self.atrous_conv_block = BRRNetAtrous(self.encoder_channels[-1], self.center_channels)
-        self.decoder = UNetDecoder(self.center_channels, self.decoder_channels, merge_way=self.merge_way,
-                                   with_bn=self.with_bn)
-        self.output = nn.Conv2d(self.decoder_channels[-1], self.num_classes, kernel_size=1)
+        self.encoder = UNetEncoder(in_channels, encoder_channels, self.with_bn)
+        self.atrous_conv_block = BRRNetAtrous(encoder_channels[-1], center_channels)
+        self.decoder = UNetDecoder(center_channels, decoder_channels,
+                                   with_bn=with_bn)
+        self.output = nn.Sequential(
+            nn.Conv2d(decoder_channels[-1], num_classes, kernel_size=1),
+            nn.ReLU(inplace=True)
+        )
 
     def forward(self, x):
         # Sequential?
@@ -34,37 +30,33 @@ class PredictModule(nn.Module):
         # print("encoder output {}".format(x.shape))
         x = self.atrous_conv_block(x)
         # print("middle output {}".format(x.shape))
-        x = self.decoder(x,x_before_pools)
+        x = self.decoder(x, x_before_pools)
         # print("decoder output {}".format(x.shape))
-        x = F.sigmoid(self.output(x))
+        x = self.output(x)
         return x
 
 
 class ResidualRefinementModule(nn.Module):
     def __init__(self, in_channels: int, center_channels: int, num_classes: int):
         super(ResidualRefinementModule, self).__init__()
-        self.in_channels = in_channels
-        self.center_channels = center_channels
-        self.num_classes = num_classes
 
         self.atrous_conv_block = BRRNetAtrous(in_channels, center_channels)
-        self.output = nn.Conv2d(center_channels, num_classes, kernel_size=3, padding=1)
-        self.bn = nn.BatchNorm2d(num_classes)
+        self.output = nn.Sequential(
+            nn.Conv2d(center_channels, num_classes, kernel_size=3, padding=1),
+            nn.BatchNorm2d(num_classes),
+            nn.ReLU(inplace=True)
+        )
 
     def forward(self, x):
         inp = x
         x = self.atrous_conv_block(x)  # does inp change?
         x = self.output(x)
-        x = F.relu(self.bn(x), inplace=True)
-        print()
         x = F.sigmoid(torch.add(inp, x))
-        print()
         return x
 
 
 class BRRNet(nn.Module):
     def __init__(self, num_classes: int):
-        # TODO: You really should to refactor these params' names.
         super(BRRNet, self).__init__()
         self.in_channels = 4
         self.encoder_channels = [64, 128, 256]
@@ -72,7 +64,7 @@ class BRRNet(nn.Module):
         self.decoder_channels = [256, 128, 64]
         self.rrm_center_channels = 64
         self.num_classes = num_classes
-        self.merge_way = 'concat'
+        # self.merge_way = 'concat'
         self.with_bn = True
 
         # Predict Module
