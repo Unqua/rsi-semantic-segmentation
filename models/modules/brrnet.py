d6c6e77c800e51197d893502da093dfbc47ec5ab
张天舒 <332755351@qq.com>
2022-01-09 14:07:30 +0800

    hopefully this unet works

--- models/modules/brrnet.py
+++ models/modules/brrnet.py
@@ -7,17 +7,15 @@ import torch.nn.functional as F
 class BRRNetAtrous(nn.Module):
     def __init__(self, in_channels: int, out_channels: int):
         super(BRRNetAtrous, self).__init__()
-        self.in_channels = in_channels
-        self.out_channels = out_channels
 
         self.atrous_convs = nn.ModuleList([])
         for i in range(6):
             self.atrous_convs.append(
                 nn.Sequential(
-                    # TODO: PAY ATTENTION TO PADDING
-                    nn.Conv2d(self.in_channels if i == 0 else self.out_channels, self.out_channels, kernel_size=3,
-                              padding=2 ** i, dilation=2 ** i),
-                    nn.BatchNorm2d(self.out_channels)
+                    # padding=dilation
+                    nn.Conv2d(in_channels if i == 0 else out_channels, out_channels, kernel_size=3,
+                              padding=2**i, dilation=2**i),
+                    nn.BatchNorm2d(out_channels)
                 )
             )
 
